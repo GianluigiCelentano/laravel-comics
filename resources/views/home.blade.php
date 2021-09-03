37a523e6f152324042a5946748d4bf0a3168c569
GianluigiCelentano <cgianluigi96@gmail.com>
2021-09-03 17:49:50 +0200

    bootstrap installato, jumbotron e comics in home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    <link rel="stylesheet" href="{{ mix('/js/app.js') }}">
-    <title>Document</title>
-</head>
+@extends('base')
 <header>
-    <ul>
-        <li>CHARACTERS</li>
-        <li>COMICS</li>
-        <li>MOVIES</li>
-        <li>TV</li>
-        <li>GAMES</li>
-        <li>COLLECTIBLES</li>
-        <li>VIDEOS</li>
-        <li>FANS</li>
-        <li>NEWS</li>
-        <li>SHOP</li>
-        <li>SEARCH</li>
-    </ul>
+    <div class="container">
+        <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+            <li>SEARCH</li>
+        </ul>
+    </div>
 </header>
 <body>
     <div class="container">
         <div class="jumbotron"></div>
-        <?php 
-        foreach($database as $comic) {
-            echo $comic["title"] . "<br>";
-            echo $comic["description"] . "<br>";
-            echo "<img src=\"{$comic["thumb"]}\"> <br>";
-            echo $comic["price"] . "<br>";
-            echo $comic["series"] . "<br>";
-            echo $comic["sale_date"] . "<br>";
-        foreach($comic["artists"] as $artist) {
-            echo $artist . " ";
-        }
-        echo "<hr>";
-        };
-        ?>
+        <div class="row">
+            <div class="col">
+                <?php 
+                foreach($database as $comic) {
+                    // echo $comic["description"] . "<br>";
+                    echo "<img class=\"comics\" src=\"{$comic["thumb"]}\">";
+                    echo $comic["title"];
+                    // echo $comic["price"] . "<br>";
+                    // echo $comic["series"] . "<br>";
+                    // echo $comic["sale_date"] . "<br>";
+                foreach($comic["artists"] as $artist) {
+                    // echo $artist . " ";
+                }
+                };
+                ?>
+            </div>
+        </div>
     </div>
 </body>
-</html>
